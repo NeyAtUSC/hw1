53fb617ac9d6efb4ddfc4bc8e94ded1ca1469a6e
Sidney Cha <neycha@usc.edu>
2025-02-01 07:22:23 +0000

    all done with part 2, the tests took too long

--- test_ulliststr.cpp
+++ test_ulliststr.cpp
@@ -9,5 +9,62 @@
 
 int main(int argc, char* argv[])
 {
+	ULListStr list;
+	std::cout << "Populating List with Push Back . . ." << std::endl;
+	list.push_back("CSCI_103");
+	std::cout << "Pushed Back: " << list.back() << std::endl;
+	list.push_back("CSCI_104");
+	std::cout << "Pushed Back: " << list.back() << std::endl;
+	list.push_back("CSCI_170");
+	std::cout << "Pushed Back: " << list.back() << std::endl;
+	list.push_back("CSCI_270");
+	std::cout << "Pushed Back: " << list.back() << std::endl;
 
+	std::cout << std::endl << "Frontmost Element: " << list.front() << std::endl;
+	std::cout << "Popped Back: " << list.back() << std::endl;
+	list.pop_back();
+    std::cout << "Back Element after Pop Back: " << list.back() << std::endl;
+	std::cout << "Popped Back: " << list.back() << std::endl;
+	list.pop_back();
+    std::cout << "Back Element after Pop Back: " << list.back() << std::endl;
+	std::cout << "Popped Back: " << list.back() << std::endl;
+	list.pop_back();
+    std::cout << "Back Element after Pop Back: " << list.back() << std::endl;
+	std::cout << "Popped Back: " << list.back() << std::endl;
+	list.pop_back();
+
+	if (list.empty()) {
+		std::cout << "List is empty after popping back all elements" << std::endl;
+	} else {
+		std::cout << "List is not empty after popping back all elements" << std::endl;
+	}
+
+	std::cout << std::endl << "Populating List with Push Front . . ." << std::endl;
+	list.push_front("CSCI_103");
+	std::cout << "Pushed Front: " << list.front() << std::endl;
+	list.push_front("CSCI_104");
+	std::cout << "Pushed Front: " << list.front() << std::endl;
+	list.push_front("CSCI_170");
+	std::cout << "Pushed Front: " << list.front() << std::endl;
+	list.push_front("CSCI_270");
+	std::cout << "Pushed Front: " << list.front() << std::endl;
+
+	std::cout << std::endl << "Backmost Element: " << list.back() << std::endl;
+	std::cout << "Popped Front: " << list.front() << std::endl;
+	list.pop_front();
+    std::cout << "Front Element after Pop Front: " << list.front() << std::endl;
+	std::cout << "Popped Front: " << list.front() << std::endl;
+	list.pop_front();
+    std::cout << "Front Element after Pop Front: " << list.front() << std::endl;
+	std::cout << "Popped Front: " << list.front() << std::endl;
+	list.pop_front();
+    std::cout << "Front Element after Pop Front: " << list.front() << std::endl;
+	std::cout << "Popped Front: " << list.front() << std::endl;
+	list.pop_front();
+
+	if (list.empty()) {
+		std::cout << "List is empty after popping front all elements" << std::endl;
+	} else {
+		std::cout << "List is not empty after popping front all elements" << std::endl;
+	}
 }
--- ulliststr.cpp
+++ ulliststr.cpp
@@ -25,6 +25,115 @@ size_t ULListStr::size() const
 }
 
 // WRITE YOUR CODE HERE
+void ULListStr::push_back(const std::string &val) {
+    if (tail_ == nullptr) {
+        tail_ = new Item();
+        head_ = tail_;
+    }
+
+    if (tail_->last < ARRSIZE) {
+        tail_->val[tail_->last] = val;
+        tail_->last++;
+    } else {
+        Item* temp = new Item();
+        temp->val[0] = val;
+        temp->last = 1;
+        temp->first = 0;
+        temp->prev = tail_;
+        tail_->next = temp;
+        tail_ = temp;
+    }
+    size_++;
+}
+void ULListStr::pop_back() {
+	if (tail_ == nullptr) {return;}
+
+	tail_->last--;
+
+	if (tail_->last == tail_->first) {
+		Item* temp = tail_;
+		tail_ = tail_->prev;
+
+		if (tail_ != nullptr) {
+			tail_->next = nullptr;
+		} else {
+			head_ = nullptr;
+		}
+
+		delete temp;
+	}
+	size_--;
+}
+void ULListStr::push_front(const std::string &val) {
+	if (head_ == nullptr) {
+		head_ = new Item();
+		head_->last = ARRSIZE;
+		head_->first = ARRSIZE;
+		tail_ = head_;
+	}
+	if (head_->first > 0) {
+        head_->val[head_->first-1] = val;
+        head_->first--;
+    } else {
+        Item* temp = new Item();
+		temp->last = ARRSIZE;
+		temp->first = ARRSIZE-1;
+        temp->val[ARRSIZE-1] = val;
+        temp->next = head_;
+        head_->prev = temp;
+        head_ = temp;
+    }
+	size_++;
+}
+void ULListStr::pop_front() {
+	if (head_ == nullptr) {return;}
+
+	head_->first++;
+
+	if (head_->first == head_->last) {
+		Item* temp = head_;
+		head_ = head_->next;
+
+		if (head_ != nullptr) {
+			head_->prev = nullptr;
+		} else {
+			tail_ = nullptr;
+		}
+
+		delete temp;
+	}
+	size_--;
+}
+std::string const & ULListStr::back() const {
+	if (tail_ == nullptr) {
+        throw std::out_of_range("List is empty");
+    }
+	return tail_->val[tail_->last-1];
+}
+std::string const & ULListStr::front() const {
+	if (head_ == nullptr) {
+        throw std::out_of_range("List is empty");
+    }
+	return head_->val[head_->first];
+}
+
+std::string* ULListStr::getValAtLoc(size_t loc) const {
+	Item* curr = head_;
+	int index = 0;
+
+	while (curr != nullptr) {
+		int itemSize = curr->last - curr->first;
+
+		if (loc < index + itemSize) {
+			return &curr->val[curr->first + (loc - index)];
+		}
+
+		index += itemSize;
+		curr = curr->next;
+	}
+
+	return nullptr;
+}
 
 void ULListStr::set(size_t loc, const std::string& val)
 {
